17b73a224ee7486f92138ee80e161fa7525cd1bd
jsorvas <59997694+jsorvas@users.noreply.github.com>
2026-05-29 11:21:16 +0200

    Refactor login tests and organize test structure

--- projet-tests/tests/auth/01_login.spec.js
+++ projet-tests/tests/auth/01_login.spec.js
@@ -8,34 +8,44 @@ test.beforeEach("Aller sur le site zotoshop", async ({ page }) => {
   await page.goto("/fr/account");
 
   // Vérification arrivée
-  await page.getByRole("heading", { name: "Bon retour" }).isVisible(true);
-  await page.getByText("Connectez-vous pour profiter").isVisible(true);
-});
-
-test("Vérification message d'erreur mail ou mot de passe", async ({ page }) => {
-  await page.getByTestId("email-input").fill("test@kl.mm");
-  await page.getByTestId("password-input").fill("test");
-  await page.getByTestId("sign-in-button").click();
-  await expect(page.getByTestId("login-error-message")).toContainText(
-    "Email ou mot de passe incorrect.",
+  await expect(page.locator('[data-testid="login-page"]')).toContainText(
+    "Bon retour",
+  );
+  await expect(page.locator('[data-testid="login-page"]')).toContainText(
+    "Connectez-vous pour profiter",
   );
 });
 
-test("Vérification du compte bloqué", async ({ page }) => {
-  await page.getByTestId("email-input").fill(idBloque);
-  await page.getByTestId("password-input").fill(password);
-  await page.getByTestId("sign-in-button").click();
-  await expect(
-    page.getByText("Ce compte est bloqué. Contactez le support."),
-  ).toBeVisible();
+test.describe("Tests avec login valide", () => {
+  test("Login valide", async ({ page }) => {
+    await page.getByTestId("email-input").fill(idValide);
+    await page.getByTestId("password-input").fill(password);
+    await page.getByTestId("sign-in-button").click();
+
+    await expect(page.locator('[data-testid="welcome-message"]')).toContainText(
+      "Bonjour Standard",
+    );
+  });
 });
 
-test("Login valide", async ({ page }) => {
-  await page.getByTestId("email-input").fill(idValide);
-  await page.getByTestId("password-input").fill(password);
-  await page.getByTestId("sign-in-button").click();
+test.describe("Tests erreurs", () => {
+  test("Vérification message d'erreur mail ou mot de passe", async ({
+    page,
+  }) => {
+    await page.getByTestId("email-input").fill("test@kl.mm");
+    await page.getByTestId("password-input").fill("test");
+    await page.getByTestId("sign-in-button").click();
+    await expect(page.getByTestId("login-error-message")).toContainText(
+      "Email ou mot de passe incorrect.",
+    );
+  });
 
-  await expect(page.locator('[data-testid="welcome-message"]')).toContainText(
-    "Bonjour Standard",
-  );
+  test("Vérification du compte bloqué", async ({ page }) => {
+    await page.getByTestId("email-input").fill(idBloque);
+    await page.getByTestId("password-input").fill(password);
+    await page.getByTestId("sign-in-button").click();
+    await expect(
+      page.getByText("Ce compte est bloqué. Contactez le support."),
+    ).toBeVisible();
+  });
 });
