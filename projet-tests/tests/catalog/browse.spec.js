22bc9e43430306c89946f321995a8741b4ae970b
jsorvas <59997694+jsorvas@users.noreply.github.com>
2026-06-10 20:55:57 +0200

    Add additional sorting test for price ascending

--- projet-tests/tests/catalog/browse.spec.js
+++ projet-tests/tests/catalog/browse.spec.js
@@ -31,7 +31,35 @@ test.describe("Catalogue zotoshop", () => {
 
   test("Trier par prix croissant", async ({ page }) => {
     await page.locator('[for="price_asc"]').click();
+    await page.waitForTimeout(500);
 
+    //Vérifier le premier élément trié
+    await expect(
+      page.locator('[data-testid="product-wrapper"]').first(),
+    ).toContainText("€15.00");
+
+    //Récupérer les prix des deux premiers éléments
+    const prix1 = await page
+      .locator('[data-testid="price"]')
+      .first()
+      .textContent();
+    const prix2 = await page
+      .locator('[data-testid="price"]')
+      .nth(1)
+      .textContent();
+
+    //Extraire les valeurs numériques des prix
+    const prix1Value = parseFloat(prix1.replace("€", "").replace(",", "."));
+    const prix2Value = parseFloat(prix2.replace("€", "").replace(",", "."));
+
+    expect(prix2Value).toBeGreaterThan(prix1Value);
+  });
+
+  test("Trier par prix croissant (2)", async ({ page }) => {
+    await page
+      .locator('[data-testid="radio-label"]')
+      .filter({ hasText: "Prix : croissant" })
+      .click();
     await page.waitForTimeout(500);
 
     //Vérifier le premier élément trié
@@ -58,7 +86,6 @@ test.describe("Catalogue zotoshop", () => {
 
   test("Trier par prix décroissant", async ({ page }) => {
     await page.locator('[for="price_desc"]').click();
-
     await page.waitForTimeout(500);
 
     //Vérifier le premier élément trié
